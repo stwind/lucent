e4469781691336c396c0ecc2eb72a484bb28070e
stwind <stwindfy@gmail.com>
2020-12-13 23:58:26 +0800

    chore: add utils

--- lucent/utils.py
+++ lucent/utils.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fontman
 import torch
 import fastprogress.fastprogress
+import torch.nn.functional as F
 from torchvision.utils import make_grid
 from PIL import Image, ImageFont, ImageDraw
+from IPython import display
 
 
 def progress_bar(**kwargs):
@@ -137,3 +139,95 @@ def fetch(url, fp=None):
 def fetch_image(url):
     resp = requests.get(url)
     return Image.open(io.BytesIO(resp.content))
+
+
+def display_pil_image(img):
+    bio = io.BytesIO()
+    img.save(bio, format="png")
+    display.display(display.Image(bio.getvalue(), format="png", retina=True))
+
+
+def binomcoeffs(n):
+    return (np.poly1d([1, 1]) ** n).coeffs
+
+
+def binomial_filter(n, channel=3):
+    coef = binomcoeffs(n - 1)
+    k = np.outer(coef, coef)
+    return np.expand_dims(k / k.sum(), (2, 3)).astype(np.float32) * np.eye(
+        channel, dtype=np.float32
+    )
+
+
+def padding_same(in_size, kernel_size, stride=1, dilation=1):
+    filter_size = (kernel_size - 1) * dilation + 1
+    out_size = (in_size + stride - 1) // stride
+    return max(0, (out_size - 1) * stride + filter_size - in_size)
+
+
+def conv_transpose2d_to(
+    x, weight, output_shape, bias=None, stride=1, dilation=1, groups=1
+):
+    """
+    same behavior as `tf.nn.conv2d_transpose`
+    """
+    oh, ow = output_shape[-2:]
+    ih, iw = x.size()[-2:]
+    kh, kw = weight.size()[-2:]
+    kh, kw = (kh - 1) * dilation + 1, (kw - 1) * dilation + 1
+    sh, sw = (stride, stride) if isinstance(stride, int) else stride
+
+    offset_h = (ih - 1) * sh + kh - oh
+    offset_w = (iw - 1) * sw + kw - ow
+    oph, opw = offset_h % 2, offset_w % 2
+
+    x = F.pad(x, [1, 1 - opw, 1, 1 - oph])
+    return F.conv_transpose2d(
+        x,
+        weight,
+        bias=bias,
+        stride=(sh, sw),
+        padding=(offset_h, offset_w),
+        output_padding=(oph, opw),
+        dilation=dilation,
+        groups=groups,
+    )
+
+
+def lap_split(img, kernel, scale=4):
+    n, c, h, w = img.size()
+
+    kh, kw = kernel.size()[-2:]
+    ph, pw = padding_same(h, kh, 2), padding_same(w, kw, 2)
+
+    x = F.pad(img, [0, pw % 2, 0, ph % 2]) if (ph % 2 or pw % 2) else img
+    lo = F.conv2d(x, kernel, stride=2, padding=(ph // 2, pw // 2))
+    lo2 = conv_transpose2d_to(lo, kernel * scale, (n, c, h, w), stride=2)
+    return lo, img - lo2
+
+
+def lap_split_n(img, kernel, n=4):
+    levels = []
+    for i in range(n):
+        img, hi = lap_split(img, kernel)
+        levels.append(hi)
+    levels.append(img)
+    return levels[::-1]
+
+
+def lap_merge(levels, kernel, scale=4):
+    img = levels[0]
+    for hi in levels[1:]:
+        img = conv_transpose2d_to(img, kernel * scale, hi.size(), stride=2) + hi
+    return img
+
+
+def normalize_std(img, eps=torch.tensor(1e-10)):
+    std = img.square().mean().sqrt()
+    return img / std.maximum(eps.to(std.device))
+
+
+def lap_normalize(img, kernel, n=4):
+    tlevels = lap_split_n(img, kernel, n)
+    tlevels = list(map(normalize_std, tlevels))
+    return lap_merge(tlevels, kernel)
