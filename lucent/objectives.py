bed66ed464fd625dd213f07e6917836626c3f6e5
stwind <stwindfy@gmail.com>
2020-12-20 15:17:59 +0800

    feat: update objectives

--- lucent/objectives.py
+++ lucent/objectives.py
@@ -1,62 +1,35 @@
 import torch
 import torch.nn as nn
 
-from lucent.viz import T
 
-
-class Objective(nn.Module):
-    def __init__(self, modules, regularizers=[]):
+class Channel(nn.Module):
+    def __init__(self, t, idx, name=0, sign=-1):
         super().__init__()
-        self.t = T(modules)
-        self.regularizers = regularizers
-
-    def regularize(self, inputs):
-        loss = sum([w * reg(inputs) for reg, w in self.regularizers])
-        return loss
-
-
-class Channel(Objective):
-    def __init__(self, module, index, sign=-1, **kwargs):
-        super().__init__([module], **kwargs)
-        self.index = index
+        self.t = t
+        self.idx = idx
+        self.name = name
         self.sign = sign
 
     def forward(self, inputs):
-        loss = self.t(0)[:, self.index].mean()
-        return self.sign * loss + self.regularize(inputs)
+        return self.sign * self.t(self.name)[:, self.idx].mean()
 
 
-class Channels(Objective):
-    def __init__(self, channels, sign=-1, **kwargs):
-        modules = [mod for mod, _, _ in channels]
-        super().__init__(modules, **kwargs)
-        self.channels = channels
-        self.sign = sign
-
-    def forward(self, inputs):
-        loss = sum(
-            [
-                self.t(i)[:, idx].mean() * w
-                for i, (_, idx, w) in enumerate(self.channels)
-            ]
-        )
-        return self.sign * loss.to(inputs.device) + self.regularize(inputs)
-
-
-class DeepDream(Objective):
-    def __init__(self, module, **kwargs):
-        super().__init__([module], **kwargs)
-
-    def forward(self, inputs):
-        return self.t(0).square().mean() + self.regularize(inputs)
-
-
-class Direction(Objective):
-    def __init__(self, module, vec, **kwargs):
-        super().__init__([module], **kwargs)
+class Direction(nn.Module):
+    def __init__(self, t, vec, name=0):
+        super().__init__()
+        self.t = t
         self.vec = vec.reshape(1, -1, 1, 1)
 
     def forward(self, inputs):
         vec = self.vec.to(inputs.device)
-        loss = torch.sum(self.t(0) * vec, 1).mean()
-        return loss + self.regularize(inputs)
+        return torch.sum(self.t(0) * vec, 1).mean()
+
+
+class DeepDream(nn.Module):
+    def __init__(self, t, name=0):
+        super().__init__()
+        self.t = t
+        self.name = name
+
+    def forward(self, inputs):
+        return self.t(0).square().mean()
